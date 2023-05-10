78a1f54735b7099066075ad4d3635b8b2dd3fa43
bhavy-deshani <deshanibhavya1730@gmail.com>
2023-05-10 12:17:21 +0530

    React

--- src/Component/ClassComponent/WelcomeTOClassComponen.jsx
+++ src/Component/ClassComponent/WelcomeTOClassComponen.jsx
@@ -3,13 +3,28 @@ import React, { Component } from 'react'
 export default class WelcomeTOClassComponent extends Component {
   render() {
     return (
+        <>
       <div>
         <h2>Welcome To Class Component</h2>
 
         <p>Class is a collection of datamember and member function only</p>
         <p>in react we have render as default method for getting output from class compo</p>
+
+
+        <h2>What is Component</h2>
+        <p>Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML. Components come in two types, Class components and Function components</p>
+
+        <h2>What is State</h2>
+
+        <p>The state is a built-in React object that is used to contain data or information about the component. A component's state can change over time; whenever it changes, the component re-renders.</p>
+
+        <h2>What is Props</h2>
+        <p>In ReactJS, the props are a type of object where the value of attributes of a tag is stored. The word “props” implies “properties”, and its working functionality is quite similar to HTML attributes. Basically, these props components are read-only components.</p>
       </div>
 
+
+
+        </>
     )
   }
 }
--- src/Component/FunctionalComponent/StyledCompoExample.jsx
+++ src/Component/FunctionalComponent/StyledCompoExample.jsx
@@ -1,28 +1,67 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import CardData from '../ClassComponent/CardData';
+import PropsClassComponent from '../ClassComponent/PropsClassComponent';
 
 const StyledCompoExample = () => {
-    const Title = styled.h1`
-    font-size: 1.5rem;
-    text-align: center;
-    color: palevioletred;
-    `;
-    const Wrapper = styled.section` 
-    padding: 4rm; 
-    background: papayawhip;`
+  const Header = styled.header`
+  height: 70px;
+  background-color: #2c3e50;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+`;
+
+const Nav = styled.nav`
+  height: 50px;
+  background-color: #3498db;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const NavLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  font-size: 18px;
+  padding: 10px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #2980b9;
+  }
+`;
+
+const Content = styled.div`
+  padding: 20px;
+`;
+
+
+
   return (
     <>
-       <Wrapper>
-                <Title>
-                    Hello World!
-                </Title>
-            </Wrapper>
-            <div className="custom-class">
-                data
-            </div>
-            <div className="custom-class" style={{backgroundColor:"green"}}>
-                data
-            </div>
+        <Header>My Website</Header>
+      <Nav>
+        <NavLink href="#">Home</NavLink>
+        <NavLink href="#">About</NavLink>
+        <NavLink href="#">Contact</NavLink>
+      </Nav>
+      <Content>
+        <h1>Welcome to my website</h1>
+        <p>This is some sample content. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet lacinia nulla ac fringilla. Etiam nec dolor tortor. Sed et urna sit amet sapien gravida molestie in eu justo. Pellentesque vitae ex nisi. Vivamus fermentum urna vitae augue ultricies, ut sodales urna consequat.</p>
+      </Content>
+
+      <div className=" col-10 mx-auto">
+                    <div className="row gy-4">
+                        {
+                            CardData.map((val, ind) => {
+                                return (<PropsClassComponent key={ind} imgSrc={val.imgSrc} prodTitle={val.prodTitle} />)
+                            })
+                        }
+                    </div>
+                </div>
     </>
   )
 }

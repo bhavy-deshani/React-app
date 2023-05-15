54e7f1b331ad2f0f661d7c5f7bade4d4158d0ca5
bhavy-deshani <deshanibhavya1730@gmail.com>
2023-05-15 15:28:58 +0530

    React

--- src/Component/FunctionalComponent/FunctionalCompoUseImp.jsx
+++ src/Component/FunctionalComponent/FunctionalCompoUseImp.jsx
@@ -2,17 +2,40 @@ import React, { useEffect, useRef, useState } from 'react'
 import FunctionalUseImpChild from './FunctionalUseImpChild';
 
 const FunctionalCompoUseImp = () => {
-    const [ele,setEle] = useState();
-    // const childRef = useRef();
+  const [ele, setEle] = useState();
+  const childRef = useRef();
 
-    const parentFunction = async (data) => {
-        console.log("called parentFunction upfate",data);
-        // await setEle(data)
-    }
+  useEffect(() => {
+  }, [])
+  const parentFunction = async (data) => {
+    console.log("called parentFunction upfate", data);
+    await setEle(data)
+  }
   return (
     <>
       {JSON.stringify(ele)}
-      <FunctionalUseImpChild onAddHandler={parentFunction} />
+      <table>
+        <thead>
+          <tr>
+            <td>col-1</td>
+            <td>col-2</td>
+          </tr>
+        </thead>
+        {(ele && ele.length) ?
+          <tbody><tr>
+            <td>{ele[0]}</td>
+            <td>{ele[1]}</td>
+          </tr>
+            <tr>
+              <td>{ele[2]}</td>
+              <td>{ele[3]}</td>
+            </tr></tbody> : <tbody><tr><td colSpan={2}>No Data Found</td></tr></tbody>}
+      </table>
+
+      <button className='btn btn-primary mt-2' onClick={() => childRef.current.getAlert()}>Shuffle</button>
+
+
+      <FunctionalUseImpChild onAddHandler={parentFunction} ref={childRef} />
     </>
   )
 }
--- src/Component/FunctionalComponent/FunctionalUseImpChild.jsx
+++ src/Component/FunctionalComponent/FunctionalUseImpChild.jsx
@@ -1,17 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 
-const FunctionalUseImpChild = (props) => {
-    const arr = [1,2,3,4,5,6]
-    useEffect(()=> {
-        props.onAddHandler(arr)
-    },[arr])
+const FunctionalUseImpChild = forwardRef( (props,ref) => {
+  const [ele, setEle] =useState(0);
+  const [ele2, setEle2] =useState(0);
+
+  useImperativeHandle(ref, () => ({
+    getAlert() {
+      console.log("called");
+    let suffRes = shuffle(arr)
+    setEle(suffRes [0])
+    suffRes.shift()
+    }
+  }))
+  // props.onAddHandler(suffRes)
+
+  useEffect(() => {
+    let suffRes = shuffle(arr)
+    console.log(suffRes);
+    setEle(suffRes[0])
+    setEle2(suffRes[1])
+    suffRes.shift()
+    suffRes.shift()
+    console.log(suffRes);
+    props.onAddHandler(suffRes)
+  },[ele])
+  
+  const arr = [1,2,3,4,5,6]
+    const shuffle = (array) => {
+      console.log("called inside shuffle");
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  };
 
 
   return (
     <>
-      
+        <h3>Child Component</h3>
+        <table>
+            <tr>
+                <td>{ele}</td>
+                <td>{ele2}</td>
+            </tr>
+        </table>
     </>
   )
-}
+})
 
 export default FunctionalUseImpChild
